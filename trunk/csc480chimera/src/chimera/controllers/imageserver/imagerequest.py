5644d9f086361ad8807cc27003e7b7a1ed2d858f
csc480class <csc480class@beae0030-f2d6-11dd-a70c-37889e090ee0>
2009-02-05 03:33:19 +0000

    uploading missed files

---
+++ trunk/csc480chimera/src/chimera/controllers/imageserver/imagerequest.py
@@ -0,0 +1,111 @@
+
+from chimera.interfaces.cameradriver import Bitpix
+from chimera.interfaces.camera import Shutter
+
+from chimera.core.exceptions import ChimeraValueError
+
+import Pyro.util
+
+import logging
+import chimera.core.log
+log = logging.getLogger(__name__)
+
+
+class ImageRequest (dict):
+    
+    def __init__(self, **kwargs):
+
+        default = {'exptime' : 1.0,
+                   'frames'  : 1,
+                   'interval': 0.0,
+                   'shutter' : Shutter.OPEN,
+                   'binning' : None,
+                   'window'  : None,
+                   'bitpix'  : Bitpix.uint16,
+                   'filename': '$DATE-$TIME',
+                   'type'    : 'object'}
+
+        # Automatically call getMetadata on all instruments + site as long as only
+        # one instance of each is listed by the manager.
+        self.auto_collect_metadata = True
+        
+        # URLs of proxies from which to get metadata before taking each image
+        self.metadataPre = []
+
+        # URLs of proxies from which to get metadata after taking each image
+        self.metadataPost = []
+        
+        # Headers accumulated during processing of each frame (=headers+metadatapre+metadatapost)
+        self.headers = []
+
+        self.update(default)
+
+        # validate keywords passed
+        not_valid = [k for k in kwargs.keys() if k not in default.keys()]
+
+        if any(not_valid):
+            if len(not_valid) > 1:
+                msg = "Invalid keywords: "
+                for k in not_valid:
+                    msg += "'%s', " % str(k)
+                msg = msg[:-2]
+
+            else:
+                msg = "Invalid keyword '%s'" % str(not_valid[0])
+
+            raise TypeError(msg)
+
+        self.update(kwargs)
+
+        # Used so that we don't take metadata twice from the same object
+        self._accum_from = []  
+    
+        # do some checkings
+        if self['exptime'] < 0.0:
+            raise ChimeraValueError('Invalid exposure length (<0 seconds).')
+        
+        # FIXME: magic number here! But we shouldn't allow arbitrary maximum for safety reasons.
+        if self['exptime'] > 12*60*60:
+            raise ChimeraValueError('Invalid exposure. Must be lower them 12 hours.')
+
+        if self['frames'] < 1:
+            raise ChimeraValueError('Invalid number of frames (<1 frame).')
+        
+        if self['interval'] < 0.0:
+            raise ChimeraValueError('Invalid interval between exposures (<0 seconds)')
+
+        if str(self['shutter']) not in Shutter:
+            raise ChimeraValueError('Invalid shutter value: ' + str(self['shutter']))
+
+    def __str__(self):
+        return ('exptime: %f, frames: %i, shutter: %s, type: %s' % (self['exptime'],
+                                                                    self['frames'],
+                                                                    self['shutter'],
+                                                                    self['type']))
+    
+    def fetchPreHeaders (self, manager):
+        auto = []
+        if self.auto_collect_metadata:
+            for cls in ('Site', 'Camera', 'Dome', 'FilterWheel', 'Focuser', 'Telescope'):
+                classes = manager.getResourcesByClass(cls)
+                if len(classes) == 1:
+                    auto.append(str(classes[0]))
+
+        self._getHeaders(manager, auto+self.metadataPre)
+
+    def fetchPostHeaders (self, manager):
+        self._getHeaders(manager, self.metadataPost)
+        
+    def _getHeaders (self, manager, proxies):
+
+        for proxyurl in proxies:
+            
+            try:
+                proxy = manager.getProxy(proxyurl)
+                proxyLoc = str(proxy.getLocation())
+                if proxyLoc not in self._accum_from:
+                    self._accum_from.append(proxyLoc)
+                    self.headers += proxy.getMetadata(self)
+            except Exception, e:
+                log.warning('Unable to get metadata from %s' % (proxyurl))
+                print ''.join(Pyro.util.getPyroTraceback(e))
---
+++ trunk/csc480chimera/src/chimera/controllers/site/main.py
@@ -0,0 +1,264 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+# chimera - observatory automation system
+# Copyright (C) 2006-2007  P. Henrique Silva <henrique@astro.ufsc.br>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+import os.path
+import logging
+import optparse
+import sys
+
+from chimera.core.location import Location
+from chimera.core.manager import Manager
+from chimera.core.systemconfig import SystemConfig
+from chimera.core.version import _chimera_version_, _chimera_description_, find_dev_version
+from chimera.core.exceptions import printException, InvalidLocationException, ChimeraException
+from chimera.core.constants import (MANAGER_DEFAULT_HOST,
+                                    MANAGER_DEFAULT_PORT,
+                                    SYSTEM_CONFIG_DEFAULT_FILENAME,
+                                    SYSTEM_CONFIG_DEFAULT_GLOBAL)
+
+from chimera.core.site import Site
+from chimera.core.path import ChimeraPath
+
+import chimera.core.log
+
+
+log = logging.getLogger(__name__)
+
+
+class SiteController (object):
+
+    def __init__(self, args = [], wait=True):
+        
+        self.wait = wait
+
+        self.options, self.args = self.parseArgs(args)
+
+        if self.options.verbose == 1:
+            chimera.core.log.setConsoleLevel(logging.INFO)
+            
+        if self.options.verbose > 1:
+            chimera.core.log.setConsoleLevel(logging.DEBUG)
+
+        self.manager = None
+
+        self.paths = {"instruments": [],
+                      "controllers": [],
+                      "drivers": []}
+
+        # add system path
+        self.paths["instruments"].append(ChimeraPath.instruments())
+        self.paths["controllers"].append(ChimeraPath.controllers())
+        self.paths["drivers"].append(ChimeraPath.drivers())
+
+    def parseArgs(self, args):
+
+        def check_location (option, opt_str, value, parser):
+            try:
+                l = Location (value)
+            except InvalidLocationException:
+                raise optparse.OptionValueError ("%s isnt't a valid location." % value)
+
+            eval ('parser.values.%s.append ("%s")' % (option.dest, value))
+
+        def check_includepath (option, opt_str, value, parser):
+
+            if not value or not os.path.isdir (os.path.abspath(value)):
+                raise optparse.OptionValueError ("Couldn't found %s include path." % value)
+
+            eval ('parser.values.%s.append ("%s")' % (option.dest, value))
+
+        parser = optparse.OptionParser(prog="chimera", version=find_dev_version() or _chimera_version_,
+                                       description=_chimera_description_,
+                                       usage="chimera --help for more information")
+
+        manag_group = optparse.OptionGroup(parser, "Basic options")
+        manag_group.add_option("-H", "--host", action="store", 
+                               dest="pyro_host", type="string",
+                               help="Host name/IP address to run as; [default=%default]",
+                               metavar="HOST")
+
+        manag_group.add_option("-P", "--port", action="store", 
+                               dest="pyro_port", type="string",
+                               help="Port on which to listen for requests; [default=%default]",
+                               metavar="PORT")
+
+        config_group = optparse.OptionGroup(parser, "Configuration")
+
+        config_group.add_option("--config", dest="config_file",
+                              help="Start Chimera using configuration from FILE.", metavar="FILE")
+
+        config_group.add_option("--skip-global", action="store_false", 
+                              dest="use_global",
+                              help="Don't use global coniguration file.")
+
+        config_group.add_option("--daemon", action="store_true", dest='daemon',
+                              help="Run Chimera in Daemon mode (will detach from current terminal).")
+
+        misc_group = optparse.OptionGroup(parser, "General")
+
+        misc_group.add_option("--dry-run", action="store_true", 
+                              dest="dry",
+                              help="Only list all configured objects (from command line and configuration files) without starting the system.")
+        
+        misc_group.add_option("-v", "--verbose", action="count", dest='verbose',
+                              help="Increase log level (multiple v's to increase even more).")
+
+        inst_group = optparse.OptionGroup(parser, "Instruments, Controllers and Drivers Management")
+
+        inst_group.add_option("-i", "--instrument", action="callback", callback=check_location,
+                              dest="instruments", type="string",
+                              help="Load the instrument defined by LOCATION."
+                              "This option could be setted many times to load multiple instruments.",
+                              metavar="LOCATION")
+
+        inst_group.add_option("-c", "--controller", action="callback", callback=check_location,
+                              dest="controllers", type="string",
+                              help="Load the controller defined by LOCATION."
+                              "This option could be setted many times to load multiple controllers.",
+                              metavar="LOCATION")
+
+        inst_group.add_option("-d", "--driver", action="callback", callback=check_location,
+                              dest="drivers", type="string",
+                              help="Load the driver defined by LOCATION."
+                              "This option could be setted many times to load multiple drivers.",
+                              metavar="LOCATION")
+
+        inst_group.add_option("-I", "--instruments-dir", action="callback", callback=check_includepath,
+                              dest="inst_dir", type="string",
+                              help="Append PATH to instruments load path.",
+                              metavar="PATH")
+
+        inst_group.add_option("-C", "--controllers-dir", action="callback", callback=check_includepath,
+                              dest="ctrl_dir", type="string",
+                              help="Append PATH to controllers load path.",
+                              metavar="PATH")
+
+        inst_group.add_option("-D", "--drivers-dir", action="callback", callback=check_includepath,
+                              dest="drv_dir", type="string",
+                              help="Append PATH to drivers load path.",
+                              metavar="PATH")
+        
+
+        parser.add_option_group(manag_group)
+        parser.add_option_group(config_group)        
+        parser.add_option_group(misc_group)
+        parser.add_option_group(inst_group)
+
+        parser.set_defaults(instruments = [],
+                            controllers = [],
+                            drivers     = [],
+                            config_file = SYSTEM_CONFIG_DEFAULT_FILENAME,
+                            inst_dir = [],
+                            ctrl_dir = [],
+                            drv_dir = [],
+                            dry=False,
+                            use_global=True,
+                            verbose = 0,
+                            daemon=False,
+                            pyro_host=MANAGER_DEFAULT_HOST,
+                            pyro_port=MANAGER_DEFAULT_PORT)
+
+        return parser.parse_args(args)
+
+    def startup(self):
+
+        if self.options.daemon:
+            # detach
+            log.info("FIXME: Daemon...")
+            
+        # system config
+        self.config = SystemConfig.fromFile(self.options.config_file, self.options.use_global)
+        
+        # manager
+        if not self.options.dry:
+            log.info("Starting system.")
+            log.info("Chimera version: %s" % find_dev_version() or _chimera_version_)
+            log.info("Chimera prefix: %s" % ChimeraPath.root())
+                
+            try:
+                self.manager = Manager(**self.config.chimera)
+            except ChimeraException, e:
+                log.error("Chimera is already running on this machine. Use chimera-admin to manage it.")
+                sys.exit(1)
+
+            log.info("Chimera: running on "+ self.manager.getHostname() + ":" + str(self.manager.getPort()))
+            if self.options.use_global:
+                log.info("Chimera: reading configuration from %s" % SYSTEM_CONFIG_DEFAULT_GLOBAL)            
+            log.info("Chimera: reading configuration from %s" % os.path.realpath(self.options.config_file))
+
+        # add site object
+        if not self.options.dry:
+            
+            for site in self.config.sites:
+                self.manager.addClass(Site, site.name, site.config, True)
+            
+        # search paths
+        log.info("Setting objects include path from command line parameters...")
+        for _dir in self.options.inst_dir:
+            self.paths["instruments"].append(_dir)
+    
+        for _dir in self.options.ctrl_dir:
+            self.paths["controllers"].append(_dir)
+        
+        for _dir in self.options.drv_dir:
+            self.paths["drivers"].append(_dir)
+
+        # init from config
+        log.info("Trying to start drivers...")
+        for drv in self.config.drivers + self.options.drivers:
+
+            if self.options.dry:
+                print drv
+            else:
+                self._add(drv, path=self.paths["drivers"], start=True)
+
+        log.info("Trying to start instruments...")
+        for inst in self.config.instruments + self.options.instruments:
+            
+            if self.options.dry:
+                print inst
+            else:
+                self._add(inst, path=self.paths["instruments"], start=True)
+
+        log.info("Trying to start controllers...")                
+        for ctrl in self.config.controllers + self.options.controllers:
+            
+            if self.options.dry:
+                print ctrl
+            else:
+                self._add(ctrl, path=self.paths["controllers"], start=True)
+
+        log.info("System up and running.")
+
+        # ok, let's wait manager work
+        if self.wait and not self.options.dry:
+            self.manager.wait()
+
+    def _add (self, location, path, start):
+        try:
+            self.manager.addLocation(location, path, start)
+        except Exception, e:
+            printException(e)
+            
+    def shutdown(self):
+        log.info("Shutting down system.")
+        self.manager.shutdown()
+
