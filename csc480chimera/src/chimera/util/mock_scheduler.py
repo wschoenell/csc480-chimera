a34007fe2c2bec08f34533a730e0e3c63b9f4501
scott.doherty777 <scott.doherty777@beae0030-f2d6-11dd-a70c-37889e090ee0>
2009-05-06 16:07:42 +0000

    adding astrometry.net code

--- csc480chimera/src/chimera/util/mock_scheduler.py
+++ csc480chimera/src/chimera/util/mock_scheduler.py
@@ -9,29 +9,28 @@ if __name__ == "__main__":
     print "Starting Mock Scheduler";
     print"========================\n"
     try:
-        print "* (1 of 3) Calling Astrometry.net script for directory of images."
-        #single image
-        #img_path = "tests/480/image/image.fits"
-        #image = AstrometryNet.path_2_image(img_path)
-
+        print "* (1) Calling Astrometry.net script for directory of images."
+        
         #batch of image
         directorypath = "tests/480/batch/"
         AstrometryNet.add_image_directory_to_queue(directorypath)
         AstrometryNet.print_queue()
-        AstrometryNet.solve_queue()
-
+        AstrometryNet.solve_queue()        
         
-        #print "testing ra and dec check."
-        #AstrometryNet.confirm_ra_dec_by_image_center(image)
         print "finished mock scheduler stage 1 processing."
     except Exception, e:
         print e
+        
     try:
-        print "\n* (2 of 3) Calling Seeing script for directory of images."
+        print "* (2) Calling Astrometry.net script for directory of images."
+        #single image
+        img_path = "tests/480/image/image.fits"
+        image = AstrometryNet.path_2_image(img_path)
+
+        print "testing ra and dec check."
+        AstrometryNet.confirm_ra_dec_by_image_center(image)
+        print "finished mock scheduler stage 1 processing."
     except Exception, e:
         print e
 
-    try:
-        print "\n* (3 of 3) Calling Extinction script for directory of images."
-    except Exception, e:
-        print e
+  
