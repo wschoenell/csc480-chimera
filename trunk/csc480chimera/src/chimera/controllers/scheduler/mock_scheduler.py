b2956836e2856062283fe063290b166b7bd380e8
scott.doherty777 <scott.doherty777@beae0030-f2d6-11dd-a70c-37889e090ee0>
2009-05-06 16:06:45 +0000

    adding astrometry.net code

---
+++ trunk/csc480chimera/src/chimera/controllers/scheduler/mock_scheduler.py
@@ -0,0 +1,36 @@
+
+#from chimera.util import run_astrometry_net
+from chimera.util.run_astrometry_net import AstrometryNet
+
+__author__="sdoherty"
+__date__ ="$May 1, 2009 12:12:12 PM$"
+
+if __name__ == "__main__":
+    print "Starting Mock Scheduler";
+    print"========================\n"
+    try:
+        print "* (1) Calling Astrometry.net script for directory of images."
+        
+        #batch of image
+        directorypath = "tests/480/batch/"
+        AstrometryNet.add_image_directory_to_queue(directorypath)
+        AstrometryNet.print_queue()
+        AstrometryNet.solve_queue()        
+        
+        print "finished mock scheduler stage 1 processing."
+    except Exception, e:
+        print e
+        
+    try:
+        print "* (2) Calling Astrometry.net script for directory of images."
+        #single image
+        img_path = "tests/480/image/image.fits"
+        image = AstrometryNet.path_2_image(img_path)
+
+        print "testing ra and dec check."
+        AstrometryNet.confirm_ra_dec_by_image_center(image)
+        print "finished mock scheduler stage 1 processing."
+    except Exception, e:
+        print e
+
+  
